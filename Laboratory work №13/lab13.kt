bfddf091946576ed03545f057c3366b0c4dbbef1
Kirill <72963968+NotOnlyButAnd@users.noreply.github.com>
2021-05-21 23:36:49 +0300

    Задание 7 ЛР13
    Чутка улучшил методы sumDigit and mulDigit (что-то немного затупил). Сделал minDigit and maxDigit

--- Laboratory work №13/lab13.kt
+++ Laboratory work №13/lab13.kt
@@ -1,13 +1,47 @@
 fun sumDigit(number:Int):Int{
     var tempNumber:Int = number
-    var digit:Int
     var sum:Int = 0
     while(tempNumber != 0){
-        digit = tempNumber%10
-        sum += digit
+        sum += tempNumber%10
         tempNumber /= 10
     }
     return sum
+}
+
+fun mulDigit(number:Int):Int{
+    var tempNumber:Int = number
+    var mul:Int = 1
+    while(tempNumber != 0){
+        mul *= tempNumber%10
+        tempNumber /= 10
+    }
+    return mul
+}
+
+fun minDigit(number:Int):Int{
+    var tempNumber:Int = number
+    var digit:Int
+    var min:Int = 9
+    while(tempNumber != 0){
+        digit = tempNumber%10
+        if (digit < min)
+            min = digit
+        tempNumber /= 10
+    }
+    return min
+}
+
+fun maxDigit(number:Int):Int{
+    var tempNumber:Int = number
+    var digit:Int
+    var max:Int = 0
+    while(tempNumber != 0){
+        digit = tempNumber%10
+        if (digit > max)
+            max = digit
+        tempNumber /= 10
+    }
+    return max
 }
 
 fun main(args: Array<String>) {
