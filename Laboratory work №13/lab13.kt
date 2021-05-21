640496dea9033633c403848a7b637a1b1994af12
Kirill <72963968+NotOnlyButAnd@users.noreply.github.com>
2021-05-21 23:15:11 +0300

    Задание 5 ЛР13
    Задание 5 Продолжение задания 3 Спросить у пользователя, какой язык у него любимый, если это Kotlin или Prolog, ответь пользователю, что он — подлиза, для других языков придумать комментарий, воспользоваться для решения задачи условным оператором и операторм when.

--- Laboratory work №13/lab13.kt
+++ Laboratory work №13/lab13.kt
@@ -1,5 +1,13 @@
-fun main() {
+fun askLanguage(Lang:String, userName:String) =
+    when{
+    Lang == "kotlin" || Lang == "prolog" -> println("\nWhat a TOADY,$userName!")
+        else -> println("\nOk, $userName, that's fine")
+    }
+
+fun main(args: Array<String>) {
     println("What's your name?")
     val name = readLine()
-    println("Hello, $name")
+    println("\nHello, $name \nWhat's your favourite language?")
+    val lang = readLine()
+    askLanguage(lang.toString(), name.toString())
 }
