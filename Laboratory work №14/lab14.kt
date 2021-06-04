3d6a140ec67289dcf2a432e63a73acbbff32eab6
Kirill <72963968+NotOnlyButAnd@users.noreply.github.com>
2021-06-04 19:38:30 +0300

    Задание 2 ЛР14
    Задание 2 Написать рекурсивную функцию с телом выражение для нахождения суммы цифр числа. Использовать рекурсию вниз. Написать рекурсивную функцию с телом выражение для нахождения суммы цифр числа на основе хвостовой рекурсии.

--- Laboratory work №14/lab14.kt
+++ Laboratory work №14/lab14.kt
@@ -1,6 +1,13 @@
 // task 1
 fun sumDigUp(number:Int):Int=  if (number != 0) number%10 + sumDigUp(number/10) else 0
 
+// task 2
+fun sumDigDown(number:Int, sum:Int):Int= if (number != 0) sumDigDown(number/10,sum + number%10) else sum
+
+tailrec fun sumDigTail(number:Int,sum:Int):Int= if (number != 0) sumDigDown(number/10,sum + number%10) else sum
+
 fun main() {
-    sumDigUp(333)
+    //print(sumDigUp(333))
+    //print(sumDigDown(122,0))
+    print(sumDigTail(312,0))
 }
